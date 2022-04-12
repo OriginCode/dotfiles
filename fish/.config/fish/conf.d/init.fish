aabce42b3c56b28109618aa8b291c29d32a4e942
Kaiyang Wu <self@origincode.me>
2022-04-12 11:05:02 -0700

    fish

---
+++ fish/.config/fish/conf.d/init.fish
@@ -0,0 +1,28 @@
+function l
+    exa -lah $argv
+end
+
+function ll
+    exa -l $argv
+end
+
+function la
+    exa -a $argv
+end
+
+function ls
+    exa $argv
+end
+
+function sysupg
+    sudo aptitude update; and sudo apt-fast full-upgrade; and sudo apt autoremove
+end
+
+function pc
+    proxychains -q $argv
+end
+
+fish_add_path $HOME/.local/bin $HOME/.cargo/bin
+
+#set fish_function_path $fish_function_path "/usr/lib/python3.8/site-packages/powerline/bindings/fish"
+#powerline-setup
