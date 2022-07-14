4ac8a7a5734295add05af9680c17bc5fb8aa779d
Kaiyang Wu <self@origincode.me>
2022-07-14 01:32:04 -0700

    fish

--- fish/.config/fish/conf.d/gnupg.fish
+++
@@ -1,7 +0,0 @@
-# Ensure that GPG Agent is used as the SSH agent
-set -e SSH_AUTH_SOCK
-set -U -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
-
-set -x GPG_TTY (tty)
-
-gpgconf --launch gpg-agent
