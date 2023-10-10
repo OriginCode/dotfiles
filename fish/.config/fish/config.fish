4ff7ca69ba8c486e657230417cad63f5506a7e1a
Kaiyang Wu <self@origincode.me>
2023-10-09 23:42:58 -0700

    20231009
    Signed-off-by: Kaiyang Wu <self@origincode.me>

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,4 +1,12 @@
-set -g fish_greeting
+#set -g fish_greeting
+
+function fish_greeting
+    set task_count (task status:pending count)
+    if test $task_count -gt 0
+        echo "You have $task_count task(s) to do!"
+        task
+    end
+end
 
 fish_vi_key_bindings
 
@@ -12,6 +20,13 @@ set -g theme_display_hostname yes
 set -g theme_display_date no
 set -g theme_show_exit_status yes
 
+set -x EDITOR vim
+
 thefuck --alias | source
 
 starship init fish | source
+
+# opam configuration
+source /home/origincode/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
+
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $PATH /home/origincode/.ghcup/bin # ghcup-env
