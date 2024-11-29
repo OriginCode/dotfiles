b7d94f75c5feecd26cb706b93db039a812ca1183
Kaiyang Wu <self@origincode.me>
2024-11-28 23:00:35 -0800

    guix: modularize, add sway
    Signed-off-by: Kaiyang Wu <self@origincode.me>

--- home.scm
+++ home.scm
@@ -10,18 +10,24 @@
              (guix gexp)
              (gnu home services)
              (gnu home services dotfiles)
-             (gnu home services shells))
+             (gnu home services shells)
+             (gnu home services sway)
+             (gnu home services xdg)
+             (gnu home services fontutils)
+             (oc configurations sway-conf)
+             (oc configurations fonts-conf))
 
 (home-environment
   ;; Below is the list of packages that will show up in your
   ;; Home profile, under ~/.guix-home/profile.
   (packages (specifications->packages
-              (list "zoxide"
-                    "racket-minimal"
-                    "vim"
-                    "fastfetch"
-                    "stow"
-                    "eza")))
+              `(,@%custom-sway-packages
+                ,@%custom-fonts-packages
+                 "fastfetch"
+                 "python-minimal"
+                 ;"stow"
+                 ;"eza"
+                 )))
 
   ;; Below is the list of Home services.  To search for available
   ;; services, run 'guix home search KEYWORD' in a terminal.
@@ -34,13 +40,12 @@
      ;           (directories (list "vim" "fastfetch"))))
 
      ;; Workaround for missing `--dotfiles` arg of stow
-     (service home-files-service-type)
      (simple-service 'vimrc-file
                      home-files-service-type
                      `((".vimrc" ,(local-file "./vim/dot-vimrc"))))
      (simple-service 'tmux-conf-file
-                     home-files-service-type
-                     `((".tmux.conf" ,(local-file "./tmux/dot-tmux.conf"))))
+                     home-xdg-configuration-files-service-type
+                     `(("tmux/tmux.conf" ,(local-file "./tmux/dot-tmux.conf"))))
      (simple-service 'gitconfig-file
                      home-files-service-type
                      `((".gitconfig"
@@ -52,26 +57,38 @@
 [user]
 	email = self@origincode.me
 	name = Kaiyang Wu"))))
-
+     (simple-service 'foot-conf-file
+                     home-xdg-configuration-files-service-type
+                     `(("foot/foot.ini" ,(local-file "./foot/dot-config/foot/foot.ini"))))
+     
      (service home-fish-service-type
               (home-fish-configuration
-                (config `(,(plain-file "zoxide-init.fish"
-                                       "zoxide init fish | source")
+                (config `(;,(plain-file "zoxide-init.fish"
+                          ;             "zoxide init fish | source")
                            ,(plain-file "local-bin-path.fish"
                                         "fish_add_path $HOME/.local/bin")
                            ,(plain-file "disable-greeting.fish"
                                         "set -g fish_greeting")))
                 (environment-variables '(("EDITOR" . "vim")
                                          ("MANWIDTH" . "80")))
-                (aliases '(("l" . "eza -lah")
-                           ("ll" . "eza -l")
-                           ("la" . "eza -a")
-                           ("lt" . "eza -lahT")
-                           ("v" . "vim")
+                (aliases '(;("l" . "eza -lah")
+                           ;("ll" . "eza -l")
+                           ;("la" . "eza -a")
+                           ;("lt" . "eza -lahT")
+                           ;("v" . "vim")
                            ("pd" . "prevd")
                            ("nd" . "nextd")
                            ("sysupg"
                             .
                             "guix pull; and sudo guix reconfigure /etc/config.scm; and guix home reconfigure dotfiles/home-configuration.scm")))
                 (abbreviations '(("gcsm" . "git commit -S -s -m")))))
+
+     (service home-sway-service-type
+              custom-sway-configuration)
+
+     (service home-xdg-user-directories-service-type)
+
+     (simple-service 'custom-fonts-conf
+                     home-fontconfig-service-type
+                     %custom-fonts-configuration)
      )))
---
+++ modules/oc/configurations/sway-conf.scm
@@ -0,0 +1,101 @@
+(define-module (oc configurations sway-conf)
+  #:export (%custom-sway-packages
+            custom-sway-configuration)
+
+  #:use-module (guix gexp)
+  #:use-module (gnu home services sway)
+  #:use-module (gnu packages terminals)
+  #:use-module (gnu packages xdisorg)
+  #:use-module (gnu packages wm))
+
+(define %custom-sway-packages
+  (list "foot"
+        "swaylock"))
+
+(define custom-sway-configuration
+  (sway-configuration
+    (variables `((mod . "Mod1")
+                 (up . "l")
+                 (down . "k")
+                 (left . "j")
+                 (right . "semicolon")
+                 (term . ,#~(string-append #$foot "/bin/foot"))
+                 ,@(map (lambda (x) `(,(string->symbol (string-append "ws" x))
+                                      . ,x))
+                       (map number->string (iota 10 1)))
+                 ))
+    (keybindings `(; launch terminal
+                   ($mod+Return . "exec $term")
+
+                   ; kill focused window
+                   ($mod+Shift+q . "kill")
+
+                   ; start fuzzel program launcher
+                   ;($mod+d . ,#~(string-append "exec " #$fuzzel "/bin/fuzzel"))
+
+                   ; change focus
+                   ($mod+$left . "focus left")
+                   ($mod+$down . "focus down")
+                   ($mod+$up . "focus up")
+                   ($mod+$right . "focus right")
+                   ($mod+Left . "focus left")
+                   ($mod+Down . "focus down")
+                   ($mod+Up . "focus up")
+                   ($mod+Right . "focus right")
+
+                   ; move focused window
+                   ($mod+Shift+$left . "move left")
+                   ($mod+Shift+$down . "move down")
+                   ($mod+Shift+$up . "move up")
+                   ($mod+Shift+$right . "move right")
+                   ($mod+Shift+Left . "move left")
+                   ($mod+Shift+Down . "move down")
+                   ($mod+Shift+Up . "move up")
+                   ($mod+Shift+Right . "move right")
+
+                   ; enter fullscreen mode for the focused container
+                   ($mod+Shift+space . "floating toggle")
+
+                   ; change focus between tiling / floating windows
+                   ($mod+space . "focus mode_toggle")
+
+                   ; move the currently focused window to the scratchpad
+                   ($mod+Shift+minus . "move scratchpad")
+
+                   ; show the next scratchpad window
+                   ($mod+minus . "scratchpad show")
+
+                   ; lock the screen
+                   ($mod+o . ,#~(string-append "exec " #$swaylock "/bin/swaylock"))
+
+                   ; resize mode
+                   ($mod+r . "mode resize")
+
+                   ; switch to workspace
+                   ,@(map (lambda (x) `(,(string->symbol (string-append "$mod+" x))
+                                        . ,(string-append "workspace number $ws" x)))
+                       (map number->string (iota 9 1)))
+                   ($mod+0 . "workspace number $ws10")
+
+                   ; move focused container to workspace
+                   ,@(map (lambda (x) `(,(string->symbol (string-append "$mod+Shift+" x))
+                                        . ,(string-append "move container to workspace number $ws" x)))
+                       (map number->string (iota 9 1)))
+                   ($mod+Shift+0 . "move container to workspace number $ws10")
+
+                   ; reload the configuration file
+                   ($mod+Shift+c . "reload")
+                   ; exit sway
+                   ($mod+Shift+e
+                     . ,#~(string-append
+                            "exec " #$sway "/bin/swaynag "
+                            "-t warning "
+                            "-m 'You pressed the exit shortcut. Do you really want to exit sway? This will end your Wayland session.' "
+                            "-B 'Yes, exit sway' "
+                            "'" #$sway "/bin/swaymsg exit'")
+                   )))
+    (bar (sway-bar
+           (status-command "while date +'%Y-%m-%d %X'; do sleep 1; done")))
+    (extra-content '("font pango:monospace 11"
+                     "floating_modifier $mod"
+                     ))))
