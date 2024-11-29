24e7edb65076c91fb72b309b787330f0eea986b5
Kaiyang Wu <self@origincode.me>
2024-11-28 23:28:06 -0800

    guix/sway: add input and screenshot conf
    Signed-off-by: Kaiyang Wu <self@origincode.me>

--- modules/oc/configurations/sway-conf.scm
+++ modules/oc/configurations/sway-conf.scm
@@ -6,96 +6,117 @@
   #:use-module (gnu home services sway)
   #:use-module (gnu packages terminals)
   #:use-module (gnu packages xdisorg)
-  #:use-module (gnu packages wm))
+  #:use-module (gnu packages wm)
+  #:use-module (gnu packages image))
 
 (define %custom-sway-packages
-  (list "foot"
-        "swaylock"))
+  (list "foot" "grim" "wl-clipboard"))
 
 (define custom-sway-configuration
   (sway-configuration
-    (variables `((mod . "Mod1")
-                 (up . "l")
-                 (down . "k")
-                 (left . "j")
-                 (right . "semicolon")
-                 (term . ,#~(string-append #$foot "/bin/foot"))
-                 ,@(map (lambda (x) `(,(string->symbol (string-append "ws" x))
-                                      . ,x))
-                       (map number->string (iota 10 1)))
-                 ))
-    (keybindings `(; launch terminal
-                   ($mod+Return . "exec $term")
+    (variables
+      `((mod . "Mod1")
+        (up . "l")
+        (down . "k")
+        (left . "j")
+        (right . "semicolon")
+        (term . ,#~(string-append #$foot "/bin/foot"))
+        ,@(map (lambda (x) `(,(string->symbol (string-append "ws" x))
+                              . ,x))
+               (map number->string (iota 10 1)))
+        ))
 
-                   ; kill focused window
-                   ($mod+Shift+q . "kill")
+    (keybindings
+      `(; launch terminal
+        ($mod+Return . "exec $term")
 
-                   ; start fuzzel program launcher
-                   ;($mod+d . ,#~(string-append "exec " #$fuzzel "/bin/fuzzel"))
+        ; kill focused window
+        ($mod+Shift+q . "kill")
 
-                   ; change focus
-                   ($mod+$left . "focus left")
-                   ($mod+$down . "focus down")
-                   ($mod+$up . "focus up")
-                   ($mod+$right . "focus right")
-                   ($mod+Left . "focus left")
-                   ($mod+Down . "focus down")
-                   ($mod+Up . "focus up")
-                   ($mod+Right . "focus right")
+        ; start fuzzel program launcher
+        ;($mod+d . ,#~(string-append "exec " #$fuzzel "/bin/fuzzel"))
 
-                   ; move focused window
-                   ($mod+Shift+$left . "move left")
-                   ($mod+Shift+$down . "move down")
-                   ($mod+Shift+$up . "move up")
-                   ($mod+Shift+$right . "move right")
-                   ($mod+Shift+Left . "move left")
-                   ($mod+Shift+Down . "move down")
-                   ($mod+Shift+Up . "move up")
-                   ($mod+Shift+Right . "move right")
+        ; change focus
+        ($mod+$left . "focus left")
+        ($mod+$down . "focus down")
+        ($mod+$up . "focus up")
+        ($mod+$right . "focus right")
+        ($mod+Left . "focus left")
+        ($mod+Down . "focus down")
+        ($mod+Up . "focus up")
+        ($mod+Right . "focus right")
 
-                   ; enter fullscreen mode for the focused container
-                   ($mod+Shift+space . "floating toggle")
+        ; move focused window
+        ($mod+Shift+$left . "move left")
+        ($mod+Shift+$down . "move down")
+        ($mod+Shift+$up . "move up")
+        ($mod+Shift+$right . "move right")
+        ($mod+Shift+Left . "move left")
+        ($mod+Shift+Down . "move down")
+        ($mod+Shift+Up . "move up")
+        ($mod+Shift+Right . "move right")
 
-                   ; change focus between tiling / floating windows
-                   ($mod+space . "focus mode_toggle")
+        ; enter fullscreen mode for the focused container
+        ($mod+Shift+space . "floating toggle")
 
-                   ; move the currently focused window to the scratchpad
-                   ($mod+Shift+minus . "move scratchpad")
+        ; change focus between tiling / floating windows
+        ($mod+space . "focus mode_toggle")
 
-                   ; show the next scratchpad window
-                   ($mod+minus . "scratchpad show")
+        ; move the currently focused window to the scratchpad
+        ($mod+Shift+minus . "move scratchpad")
 
-                   ; lock the screen
-                   ($mod+o . ,#~(string-append "exec " #$swaylock "/bin/swaylock"))
+        ; show the next scratchpad window
+        ($mod+minus . "scratchpad show")
 
-                   ; resize mode
-                   ($mod+r . "mode resize")
+        ; lock the screen
+        ;($mod+o . ,#~(string-append "exec " #$swaylock "/bin/swaylock"))
 
-                   ; switch to workspace
-                   ,@(map (lambda (x) `(,(string->symbol (string-append "$mod+" x))
-                                        . ,(string-append "workspace number $ws" x)))
-                       (map number->string (iota 9 1)))
-                   ($mod+0 . "workspace number $ws10")
+        ; resize mode
+        ($mod+r . "mode resize")
 
-                   ; move focused container to workspace
-                   ,@(map (lambda (x) `(,(string->symbol (string-append "$mod+Shift+" x))
-                                        . ,(string-append "move container to workspace number $ws" x)))
-                       (map number->string (iota 9 1)))
-                   ($mod+Shift+0 . "move container to workspace number $ws10")
+        ; switch to workspace
+        ,@(map (lambda (x) `(,(string->symbol (string-append "$mod+" x))
+                              . ,(string-append "workspace number $ws" x)))
+               (map number->string (iota 9 1)))
+        ($mod+0 . "workspace number $ws10")
 
-                   ; reload the configuration file
-                   ($mod+Shift+c . "reload")
-                   ; exit sway
-                   ($mod+Shift+e
-                     . ,#~(string-append
-                            "exec " #$sway "/bin/swaynag "
-                            "-t warning "
-                            "-m 'You pressed the exit shortcut. Do you really want to exit sway? This will end your Wayland session.' "
-                            "-B 'Yes, exit sway' "
-                            "'" #$sway "/bin/swaymsg exit'")
-                   )))
+        ; move focused container to workspace
+        ,@(map (lambda (x) `(,(string->symbol (string-append "$mod+Shift+" x))
+                              . ,(string-append "move container to workspace number $ws" x)))
+               (map number->string (iota 9 1)))
+        ($mod+Shift+0 . "move container to workspace number $ws10")
+
+        ; reload the configuration file
+        ($mod+Shift+c . "reload")
+        ; exit sway
+        ($mod+Shift+e
+          . ,#~(string-append
+                 "exec " #$sway "/bin/swaynag "
+                 "-t warning "
+                 "-m 'You pressed the exit shortcut. Do you really want to exit sway? This will end your Wayland session.' "
+                 "-B 'Yes, exit sway' "
+                 "'" #$sway "/bin/swaymsg exit'"))
+
+        ; screenshots
+        (Print
+          . ,#~(string-append "exec " #$grim "/bin/grim " "~/Pictures/screenshot.png"))
+        (Ctrl+Print
+          . ,#~(string-append "exec " #$grim "/bin/grim - | " #$wl-clipboard "/bin/wl-copy"))
+        ))
+
+    (startup-programs `(,#~(string-append #$foot "/bin/foot")))
+
     (bar (sway-bar
            (status-command "while date +'%Y-%m-%d %X'; do sleep 1; done")))
+
+    (inputs (list
+              (sway-input
+                (identifier "9610:30:Pine64_Pinebook_Pro_Touchpad")
+                (disable-while-typing #t)
+                (tap #t)
+                (extra-content '("natural_scroll enabled"
+                                 "middle_emulation enabled")))))
+
     (extra-content '("font pango:monospace 11"
                      "floating_modifier $mod"
                      ))))
